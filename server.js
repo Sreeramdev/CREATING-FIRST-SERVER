80964560148676258a9cc14d5d5f2fb4e5ec996c
Sreeram-1 <sreeramdevendra52@gmail.com>
2024-06-01 03:28:13 +0530

    j

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require("express")
+const express = require("express");
 
 const app = express();
 
@@ -8,26 +8,25 @@ const port = 8081;
 
 const toDoList = ["node Byte has ex", "Angelina jolie"];
 
-app.get("/todos",(req,res)=>{
-    res.status(200).send(toDoList);
-})
-app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`);
-})
+app.get("/todos", (req, res) => {
+  res.status(200).send(toDoList);
+});
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
-app.post("/todos",(req,res)=>{
-    let newToDo = toDoList;
-    newToDo.push(req.body.item);
-    res.status(200).send({"message": "item added"});
-})
+app.post("/todos", (req, res) => {
+  let newToDo = toDoList;
+  newToDo.push(req.body.item);
+  res.status(200).send({ message: "item added" });
+});
 
-app.delete("/todos",(req,res)=>{
-    const itemToDelete = req.body.item;
-    toDoList.find((element,index)=>{
-        if(element === itemToDelete){
-            toDoList.splice(index,1)
-        }
-
-    })
-    res.status(200).send({message : "item deleted succesfully"})
-})
+app.delete("/todos", (req, res) => {
+  const itemToDelete = req.body.item;
+  toDoList.find((element, index) => {
+    if (element === itemToDelete) {
+      toDoList.splice(index, 1);
+    }
+  });
+  res.status(200).send({ message: "item deleted succesfully" });
+});
