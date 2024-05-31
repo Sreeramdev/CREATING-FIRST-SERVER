2d2550b8c7da48c055149b090b1499bb3d5e38e0
Sreeram-1 <sreeramdevendra52@gmail.com>
2024-06-01 00:36:17 +0530

    feat: add nodemon dependency and functionality
    Adds nodemon as a dependency to enable automatic server restarts on code changes.
    
    Updates server.js to use nodemon for development, allowing for faster development cycles.
    
    Additionally, adds a TODO endpoint to the server for viewing and adding to a list of TODO items.

--- server.js
+++ server.js
@@ -1,13 +1,36 @@
-const http = require('http');
+const http = require("http");
 
 const port = 8081;
+const toDolist = ["node is working", "playing with code"];
+http
+  .createServer((request, response) => {
+    const { method, url } = request;
+    if (url === "/todos"){
+    if (method === "GET"){
+        response.writeHead(200);
+        response.write(toDolist.toString())
+    
+    }else if(method === "POST"){
+        let body = '';
+        request.on('error',()=>{
+        console.error('error')
+        }).on('data',(chunk)=>{
+         body += chunk;
+        }).on("end",()=>{
+            body = JSON.parse(body);
+            console.log(body);
+        })
+    }
+    {
+        response.writeHead(200);
+    }
 
-http.createServer((request,response) =>{
-    response.writeHead(200,{"Content-Type": "text/html"})
-    response.write("<h1>Hello World this is from server</h1>")
-    response.end()
-})
+}else{
+    response.writeHead(501);
+}
+    response.end();
+  })
 
-.listen(port, ()=>{
+  .listen(port, () => {
     console.log(`Server is running on port ${port}`);
-})
+  });
